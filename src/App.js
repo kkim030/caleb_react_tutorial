267f737026adefd395f23899320a872ab4432ffd
Kelly Kim <93014710+kkim030@users.noreply.github.com>
2023-01-11 13:24:25 -0500

    component exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
+  console.log("we are about to list employees");
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log("inside the return")}
+      {showEmployees ? (
+        <>
+          <Employee />
+        </>
+      ) : (
+        <p> You cannot see the employees </p>
+      )}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee() {
+  return <h3> here is an Employee</h3>;
+}
+
+export default Employee;
