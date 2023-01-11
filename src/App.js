c93fee6ba25063c9a622cfcb4af2b26d3841c603
Kelly Kim <93014710+kkim030@users.noreply.github.com>
2023-01-11 14:29:51 -0500

    add css components using tail-wind

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
-import "./App.css";
+import "./index.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("dev");
   const showEmployees = true;
   return (
     <div className="App">
       {console.log("inside the return")}
       {showEmployees ? (
         <>
-          <Employee name="Caleb" role="intern" />
-          <Employee name="Abby" />
-          <Employee name="Caleb" role="intern" />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+            <Employee
+              name="Caleb"
+              role="intern"
+              img="https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg"
+            />
+          </div>
         </>
       ) : (
         <p> You cannot see the employees </p>
