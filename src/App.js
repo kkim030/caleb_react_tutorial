4c51fca6eeaaadec9d01639363c760429bf32df3
Kelly Kim <93014710+kkim030@users.noreply.github.com>
2023-01-12 22:36:20 -0500

    Finished Family Directory - added update profile

--- src/App.js
+++ src/App.js
@@ -7,31 +7,46 @@ function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Kelly",
       role: "Daughter1",
       img: "https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg",
     },
     {
+      id: 2,
       name: "Min",
       role: "Daughter2",
       img: "https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg",
     },
     {
+      id: 3,
       name: "Mom",
       role: "Mommy",
       img: "https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg",
     },
     {
+      id: 4,
       name: "Dad",
       role: "Daddy",
       img: "https://images.pexels.com/photos/15031715/pexels-photo-15031715.jpeg",
     },
     {
+      id: 5,
       name: "Cupcake",
       role: "PotentialDog<3",
       img: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -46,13 +61,14 @@ function App() {
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              console.log(employee);
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
   return (
-    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <img
         className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
         src={props.img}
@@ -13,7 +13,12 @@ function Employee(props) {
           <p className="text-slate-500 font-medium">{props.role}</p>
         </div>
 
-        <EditEmployee />
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          role={props.role}
+          updateEmployee={props.updateEmployee}
+        />
       </div>
     </div>
   );
